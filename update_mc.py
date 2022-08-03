e9ab0a6e8d9b8a2830f93e6f219eb59320c3a6f1
Daryl Styrk <darylstyrk@gmail.com>
2022-08-03 16:41:20 -0500

    Initial commit

---
+++ update_mc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  2 19:31:01 2022
+
+@author: darylstyrk
+"""
+
+import requests
+
+versions = requests.get('https://launchermeta.mojang.com/mc/game/version_manifest.json')
+current= versions.json()['versions'][0]['url']
+
+target = requests.get(current)
+target_dl = target.json()['downloads']['server']['url']
+
+URL = target_dl
+
+r = requests.get(URL, allow_redirects=True)
+
+with open('minecraft_server.jar', 'wb') as file:
+    file.write(r.content)
