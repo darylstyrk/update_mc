04d9d5ed836df9efd26d1dc51ba3eb50019ba7ab
Daryl Styrk <darylstyrk@gmail.com>
2022-08-10 18:37:44 -0500

    This is now comparing the local and remote sha1's to decide if it is required to download a newer version. Also ran it through flake8 and black odules. Looks fine to me.

--- update_mc.py
+++ update_mc.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
 import requests
+import hashlib
 
-versions = requests.get('https://launchermeta.mojang.com/mc/game/version_manifest.json')
-current = versions.json()['versions'][0]['url']
-
+versions = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+current = versions.json()["versions"][0]["url"]
 target = requests.get(current)
-target_dl = target.json()['downloads']['server']['url']
+target_dl = target.json()["downloads"]["server"]["url"]
+remote_sha1 = target.json()["downloads"]["server"]["sha1"]
 
-URL = target_dl
+BUF_SIZE = 65536
 
-r = requests.get(URL, allow_redirects=True)
+sha1 = hashlib.sha1()
+with open("/usr/local/bin/minecraft_server.jar", "rb") as local_sha1:
+    while True:
+        data = local_sha1.read(BUF_SIZE)
+        if not data:
+            break
+        sha1.update(data)
+local_sha1 = sha1.hexdigest()
 
-with open('/usr/local/bin/minecraft_server.jar', 'wb') as file:
-    file.write(r.content)
+if remote_sha1 != local_sha1:
+    remote_jar = requests.get(target_dl, allow_redirects=True)
+    with open("/usr/local/bin/minecraft_server.jar", "wb") as file:
+        file.write(remote_jar.content)
