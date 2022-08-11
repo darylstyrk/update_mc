4a7bdf51aab1ba339adcc0c2031679fe52fa6641
Daryl Styrk <darylstyrk@gmail.com>
2022-08-11 12:55:31 -0500

    Replaced requests with httpx.

--- update_mc.py
+++ update_mc.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
 
-import requests
+import httpx
 import hashlib
 
-versions = requests.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
+versions = httpx.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
 current = versions.json()["versions"][0]["url"]
-target = requests.get(current)
+target = httpx.get(current)
 target_dl = target.json()["downloads"]["server"]["url"]
 remote_sha1 = target.json()["downloads"]["server"]["sha1"]
-
-BUF_SIZE = 65536
 
 sha1 = hashlib.sha1()
 with open("/usr/local/bin/minecraft_server.jar", "rb") as local_sha1:
     while True:
-        data = local_sha1.read(BUF_SIZE)
+        data = local_sha1.read(65536)
         if not data:
             break
         sha1.update(data)
 local_sha1 = sha1.hexdigest()
 
 if remote_sha1 != local_sha1:
-    remote_jar = requests.get(target_dl, allow_redirects=True)
+    remote_jar = httpx.get(target_dl)
     with open("/usr/local/bin/minecraft_server.jar", "wb") as file:
         file.write(remote_jar.content)
