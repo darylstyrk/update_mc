0449ad4edfb4a1d8179f3eda31576f450180c24a
Daryl Styrk <darylstyrk@gmail.com>
2022-10-04 20:11:12 -0500

    Adding a sleep in here to avoid a thundering herd.

--- update_mc.py
+++ update_mc.py
@@ -3,6 +3,10 @@
 import httpx
 import hashlib
 import os
+from random import randint
+from time import sleep
+
+sleep(randint(60,3600))
 
 versions = httpx.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
 current = versions.json()["versions"][0]["url"]
@@ -10,8 +14,8 @@ target = httpx.get(current)
 target_dl = target.json()["downloads"]["server"]["url"]
 remote_sha1 = target.json()["downloads"]["server"]["sha1"]
 
-if not os.path.exists('/usr/local/bin/minecraft_server.jar'):
-    os.mknod('/usr/local/bin/minecraft_server.jar')
+if not os.path.exists("/usr/local/bin/minecraft_server.jar"):
+    os.mknod("/usr/local/bin/minecraft_server.jar")
 
 sha1 = hashlib.sha1()
 with open("/usr/local/bin/minecraft_server.jar", "rb") as local_sha1:
