72abc339c8fafdbc871b02bf5a7c8fb271f49f62
Daryl Styrk <darylstyrk@gmail.com>
2023-05-07 09:40:41 -0500

    Couple of changes after some ChatGPT suggestions.

--- update_mc.py
+++ update_mc.py
@@ -3,6 +3,7 @@
 import httpx
 import hashlib
 import os
+import tempfile
 from random import randint
 from time import sleep
 
@@ -14,20 +15,22 @@ target = httpx.get(current)
 target_dl = target.json()["downloads"]["server"]["url"]
 remote_sha1 = target.json()["downloads"]["server"]["sha1"]
 
-if not os.path.exists("/usr/local/bin/minecraft_server.jar"):
-    os.mknod("/usr/local/bin/minecraft_server.jar")
+local_file = "/usr/local/bin/minecraft_server.jar"
+if not os.path.exists(local_file):
+    open(local_file, "w").close()
 
 sha1 = hashlib.sha1()
-with open("/usr/local/bin/minecraft_server.jar", "rb") as local_sha1:
+with open(local_file, "rb") as f:
     while True:
-        data = local_sha1.read(65536)
+        data = f.read(65536)
         if not data:
             break
         sha1.update(data)
-local_sha1 = sha1.hexdigest()
+local_sha1 = sha1.digest()
 
 if remote_sha1 != local_sha1:
     remote_jar = httpx.get(target_dl)
-    with open("/usr/local/bin/minecraft_server.jar", "wb") as file:
-        file.write(remote_jar.content)
-        os.chmod("/usr/local/bin/minecraft_server.jar", 0o755)
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        f.write(remote_jar.content)
+    os.chmod(f.name, 0o755)
+    os.replace(f.name, local_file)
