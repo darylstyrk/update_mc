7dbd8cf4f0dea04f8f56986b4c4cf30eda45cbc9
Daryl Styrk <darylstyrk@gmail.com>
2023-05-07 09:53:15 -0500

    Adding comments.

--- update_mc.py
+++ update_mc.py
@@ -7,18 +7,22 @@ import tempfile
 from random import randint
 from time import sleep
 
+# Run sometime in the next hour to avoid a herd at Mojang.
 sleep(randint(60, 3600))
 
+# Go get a list of all versions and start cutting it up to the specific URL and hash we need.
 versions = httpx.get("https://launchermeta.mojang.com/mc/game/version_manifest.json")
 current = versions.json()["versions"][0]["url"]
 target = httpx.get(current)
 target_dl = target.json()["downloads"]["server"]["url"]
 remote_sha1 = target.json()["downloads"]["server"]["sha1"]
 
+# If it's a new install we create an empty file.
 local_file = "/usr/local/bin/minecraft_server.jar"
 if not os.path.exists(local_file):
     open(local_file, "w").close()
 
+# Get the local hash to compare to the remote hash.
 sha1 = hashlib.sha1()
 with open(local_file, "rb") as f:
     while True:
@@ -28,6 +32,8 @@ with open(local_file, "rb") as f:
         sha1.update(data)
 local_sha1 = sha1.digest()
 
+# Compare the remote hash to the local hash. If they're not the same we assume the remote has is
+# newer and we replace the local version with the remote.
 if remote_sha1 != local_sha1:
     remote_jar = httpx.get(target_dl)
     with tempfile.NamedTemporaryFile(delete=False) as f:
